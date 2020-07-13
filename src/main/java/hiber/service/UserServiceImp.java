88f7106b90b1d3dd96cc47628bc481ca3d0344ea
Zkoiran <122123yc>
2020-07-13 22:23:51 +0700

    Done by Ilya Krasavin

---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,38 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Queue;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Transactional
+   @Override
+   public User get(Car car) {
+      return userDao.get(car);
+   }
+
+}
